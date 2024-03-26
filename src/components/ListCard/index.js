cf81913d33246679b42ed75d10655eea9ad8aa8a
Rafael-Almeida-Dev <rafael100almeida@gmail.com>
2024-03-25 23:49:22 -0300

    feat: creat final layout the list

--- src/components/ListCard/index.js
+++ src/components/ListCard/index.js
@@ -1,9 +1,25 @@
+import { useState } from "react";
 import "./index.css";
 
-export const ListCard = (props) => {
+export const ListCard = ({ item }) => {
+  const [checked, setChecked] = useState(false);
+
   return (
-    <div className="list-card-container ">
-      <span>{props.item.name}</span>
+    <div className="list-card-container">
+      <div className="checkbox">
+        <input
+          onChange={() => setChecked(!checked)}
+          checked={checked}
+          id="checkbox"
+          type="checkbox"
+        />
+        <label htmlFor="checkbox" />
+      </div>
+      <div className="list-card-text-container">
+        <span className="list-card-title ">{item?.name}</span>
+        <span className="list-card-subtitle">{item?.quantity} unidade</span>
+      </div>
+      <img src="/images/arrow.svg" alt="arrow-icon" className="arrow-icon" />
     </div>
   );
 };
